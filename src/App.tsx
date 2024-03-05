f63ce3e5cc341548c8fb071e50484c3055521b2c
Paloma Rodrigues <palomarodrigues.0013@gmail.com>
2024-03-05 18:53:39 -0300

    feat: add header and footer components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import { Footer } from './components/footer'
+import { Header } from './components/header'
+
 export function App() {
-  return <h1>Hello world!</h1>
+  return (
+    <main className="flex flex-col justify-between px-[50px] min-h-screen bg-grey-600">
+      <Header />
+      <Footer />
+    </main>
+  )
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,14 @@
+export function Footer() {
+  return (
+    <footer>
+      <div className="border border-grey-300" />
+
+      <p className="text-grey-100 text-sm text-center p-[20px]">
+        developed with 💚 by{' '}
+        <a href="" className="underline hover:text-grey-100/75 duration-200">
+          Paloma Rodrigues
+        </a>
+      </p>
+    </footer>
+  )
+}
