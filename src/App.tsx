ed0d542ae424664f074464b91a273ba40f98a20d
Paloma Rodrigues <palomarodrigues.0013@gmail.com>
2024-03-05 18:58:47 -0300

    feat: add description component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { Footer } from './components/footer'
 import { Header } from './components/header'
+import { Footer } from './components/footer'
+import { Description } from './components/description'
 
 export function App() {
   return (
     <main className="flex flex-col justify-between px-[50px] min-h-screen bg-grey-600">
       <Header />
+      <Description />
       <Footer />
     </main>
   )
---
+++ src/components/description.tsx
@@ -0,0 +1,14 @@
+export function Description() {
+  return (
+    <section className="flex flex-col items-center gap-[4px]">
+      <h2 className="text-xl lg:text-2xl text-white-100 font-semibold">
+        Image to Text Converter with <span className="text-mint-200">AI</span>
+      </h2>
+
+      <p className="text-grey-100 text-sm lg:text-base text-center leading-relaxed">
+        Convert images to text with ease! Upload your image and get converted text
+        instantly with a simple tap.
+      </p>
+    </section>
+  )
+}
