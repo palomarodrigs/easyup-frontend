0127190cd7140024b8a619eee8c5c9d9e7512a9e
Paloma Rodrigues <palomarodrigues.0013@gmail.com>
2024-03-07 15:41:08 -0300

    style: change padding on desktop

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,6 @@
 export function Header() {
   return (
-    <header className="py-[50px]">
+    <header className="py-[70px] lg:py-[50px]">
       <h1 className="font-bold text-3xl text-mint-200">
         easy<span className="text-white-100">up</span>
       </h1>
